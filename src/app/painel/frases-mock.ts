18ec80391d6196f16bfaf018fd4cbd582c677a39
raulanacleto <iraul@msn.com>
2019-08-14 09:51:42 -0300

    trabalhando com data binding: string interpolation, property e event

---
+++ src/app/painel/frases-mock.ts
@@ -0,0 +1,8 @@
+import { Frase } from '../shared/frase.model'
+
+export const FRASES: Frase[] = [
+    { fraseEng: 'i like to learn', frasePtBr: 'eu gosto de aprender'},
+    { fraseEng: 'i watch tv', frasePtBr: 'eu assisto tv'},
+    { fraseEng: 'how are you', frasePtBr: 'como vai voce'},
+    { fraseEng: 'i eat bread', frasePtBr: 'eu comopao'},
+]
---
+++ src/app/shared/frase.model.ts
@@ -0,0 +1,14 @@
+export class Frase {
+/*     public fraseEng: string
+    public frasePtBr: string
+
+    constructor(fraseEng: string, frasePtBr: string){
+        this.fraseEng = fraseEng
+        this.frasePtBr = frasePtBr
+    }
+ */
+
+    //todo o bloco comentado acima pode ser escrito assim:
+    constructor(public fraseEng: string, public frasePtBr: string){}
+
+}
